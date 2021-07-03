ee7b817383a92737b9a080f4f9ba172822f082db
acholonu <ucacholonu@hotmail.com>
2021-07-03 10:49:48 -0500

    Updated response hello world application

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,30 @@
+import typing
 from flask import Flask
+from flask import json
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/metric")
+def get_metrics():
+    response = app.response_class(
+        response = json.dumps('{"UserCount": 140, "UserCountActive": 23}'), # return data
+        status=200, #everything is good
+        mimetype='application/json'
+    )
+    return response
+
+@app.route("/status")
+def get_status():
+    # API Return call for the web response
+    response = app.response_class(
+        response = json.dumps({"result": "OK - healthy"}), # return data
+        status=200, #everything is good
+        mimetype='application/json'
+    )
+    return response
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
